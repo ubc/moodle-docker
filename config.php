87dd1e60fd12bf2a64a31770ed7260ee59c92d41
Pan Luo <pan.luo@ubc.ca>
2019-04-04 23:01:36 -0700

    Add MOODLE_DISABLE_UPDATE_AUTODEPLOY and MOODLE_DEBUG

--- config.php
+++ config.php
@@ -486,7 +486,7 @@ $CFG->localcachedir = '/var/local/cache';      // Intended for local node cachin
 // features (such as cancelling the plugin installation or upgrade) via the
 // server administration web interface.
 //
-//      $CFG->disableupdateautodeploy = true;
+$CFG->disableupdateautodeploy = filter_var(loadenv('MOODLE_DISABLE_UPDATE_AUTODEPLOY', false), FILTER_VALIDATE_BOOLEAN);
 //
 // Use the following flag to disable the warning on the system notifications page
 // about present development libraries. This flag will not disable the warning within
@@ -626,69 +626,70 @@ $CFG->localcachedir = '/var/local/cache';      // Intended for local node cachin
 //
 // Force a debugging mode regardless the settings in the site administration
 // @error_reporting(E_ALL | E_STRICT); // NOT FOR PRODUCTION SERVERS!
-// @ini_set('display_errors', '1');    // NOT FOR PRODUCTION SERVERS!
-// $CFG->debug = (E_ALL | E_STRICT);   // === DEBUG_DEVELOPER - NOT FOR PRODUCTION SERVERS!
-// $CFG->debugdisplay = 1;             // NOT FOR PRODUCTION SERVERS!
-//
-// You can specify a comma separated list of user ids that that always see
-// debug messages, this overrides the debug flag in $CFG->debug and $CFG->debugdisplay
-// for these users only.
-// $CFG->debugusers = '2';
-//
-// Prevent theme caching
-// $CFG->themedesignermode = true; // NOT FOR PRODUCTION SERVERS!
-//
-// Enable verbose debug information during fetching of email messages from IMAP server.
-// $CFG->debugimap = true;
-//
-// Prevent JS caching
-// $CFG->cachejs = false; // NOT FOR PRODUCTION SERVERS!
-//
-// Restrict which YUI logging statements are shown in the browser console.
-// For details see the upstream documentation:
-//   http://yuilibrary.com/yui/docs/api/classes/config.html#property_logInclude
-//   http://yuilibrary.com/yui/docs/api/classes/config.html#property_logExclude
-// $CFG->yuiloginclude = array(
-//     'moodle-core-dock-loader' => true,
-//     'moodle-course-categoryexpander' => true,
-// );
-// $CFG->yuilogexclude = array(
-//     'moodle-core-dock' => true,
-//     'moodle-core-notification' => true,
-// );
-//
-// Set the minimum log level for YUI logging statements.
-// For details see the upstream documentation:
-//   http://yuilibrary.com/yui/docs/api/classes/config.html#property_logLevel
-// $CFG->yuiloglevel = 'debug';
-//
-// Prevent core_string_manager application caching
-// $CFG->langstringcache = false; // NOT FOR PRODUCTION SERVERS!
-//
-// When working with production data on test servers, no emails or other messages
-// should ever be send to real users
-// $CFG->noemailever = true;    // NOT FOR PRODUCTION SERVERS!
-//
-// Divert all outgoing emails to this address to test and debug emailing features
-// $CFG->divertallemailsto = 'root@localhost.local'; // NOT FOR PRODUCTION SERVERS!
-//
-// Except for certain email addresses you want to let through for testing. Accepts
-// a comma separated list of regexes.
-// $CFG->divertallemailsexcept = 'tester@dev.com, fred(\+.*)?@example.com'; // NOT FOR PRODUCTION SERVERS!
-//
-// Uncomment if you want to allow empty comments when modifying install.xml files.
-// $CFG->xmldbdisablecommentchecking = true;    // NOT FOR PRODUCTION SERVERS!
-//
-// Since 2.0 sql queries are not shown during upgrade by default.
-// Please note that this setting may produce very long upgrade page on large sites.
-// $CFG->upgradeshowsql = true; // NOT FOR PRODUCTION SERVERS!
-//
-// Add SQL queries to the output of cron, just before their execution
-// $CFG->showcronsql = true;
-//
-// Force developer level debug and add debug info to the output of cron
-// $CFG->showcrondebugging = true;
-//
+if (getenv('MOODLE_DEBUG')) {
+    @ini_set('display_errors', '1');    // NOT FOR PRODUCTION SERVERS!
+    $CFG->debug = (E_ALL | E_STRICT);   // === DEBUG_DEVELOPER - NOT FOR PRODUCTION SERVERS!
+    $CFG->debugdisplay = 1;             // NOT FOR PRODUCTION SERVERS!
+    //
+    // You can specify a comma separated list of user ids that that always see
+    // debug messages, this overrides the debug flag in $CFG->debug and $CFG->debugdisplay
+    // for these users only.
+    $CFG->debugusers = '2';
+    //
+    // Prevent theme caching
+    $CFG->themedesignermode = true; // NOT FOR PRODUCTION SERVERS!
+    //
+    // Enable verbose debug information during fetching of email messages from IMAP server.
+    $CFG->debugimap = true;
+    //
+    // Prevent JS caching
+    $CFG->cachejs = false; // NOT FOR PRODUCTION SERVERS!
+    //
+    // Restrict which YUI logging statements are shown in the browser console.
+    // For details see the upstream documentation:
+    //   http://yuilibrary.com/yui/docs/api/classes/config.html#property_logInclude
+    //   http://yuilibrary.com/yui/docs/api/classes/config.html#property_logExclude
+    $CFG->yuiloginclude = array(
+        'moodle-core-dock-loader' => true,
+        'moodle-course-categoryexpander' => true,
+    );
+    $CFG->yuilogexclude = array(
+        'moodle-core-dock' => true,
+        'moodle-core-notification' => true,
+    );
+    //
+    // Set the minimum log level for YUI logging statements.
+    // For details see the upstream documentation:
+    //   http://yuilibrary.com/yui/docs/api/classes/config.html#property_logLevel
+    $CFG->yuiloglevel = 'debug';
+    //
+    // Prevent core_string_manager application caching
+    $CFG->langstringcache = false; // NOT FOR PRODUCTION SERVERS!
+    //
+    // When working with production data on test servers, no emails or other messages
+    // should ever be send to real users
+    $CFG->noemailever = true;    // NOT FOR PRODUCTION SERVERS!
+    //
+    // Divert all outgoing emails to this address to test and debug emailing features
+    $CFG->divertallemailsto = 'root@localhost.local'; // NOT FOR PRODUCTION SERVERS!
+    //
+    // Except for certain email addresses you want to let through for testing. Accepts
+    // a comma separated list of regexes.
+    $CFG->divertallemailsexcept = 'tester@dev.com, fred(\+.*)?@example.com'; // NOT FOR PRODUCTION SERVERS!
+    //
+    // Uncomment if you want to allow empty comments when modifying install.xml files.
+    $CFG->xmldbdisablecommentchecking = true;    // NOT FOR PRODUCTION SERVERS!
+    //
+    // Since 2.0 sql queries are not shown during upgrade by default.
+    // Please note that this setting may produce very long upgrade page on large sites.
+    $CFG->upgradeshowsql = true; // NOT FOR PRODUCTION SERVERS!
+    //
+    // Add SQL queries to the output of cron, just before their execution
+    $CFG->showcronsql = true;
+    //
+    // Force developer level debug and add debug info to the output of cron
+    $CFG->showcrondebugging = true;
+}
 //=========================================================================
 // 8. FORCED SETTINGS
 //=========================================================================
