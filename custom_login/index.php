c2c7664fe4e986b98cf268a052abd23eed1cafb3
Tyler Cinkant <tcinkant@gmail.com>
2019-07-23 14:05:17 -0700

    Adding self-signup

--- custom_login/index.php
+++ custom_login/index.php
@@ -26,7 +26,7 @@ $login_token = \core\session\manager::get_login_token();
 <meta name="author" content="">
 <link href="https://cdn.ubc.ca/clf/7.0.4/css/ubc-clf-full.min.css" rel="stylesheet">
 <!-- Stylesheets -->
-<link rel="stylesheet" href="./files/styles.css">
+<link rel="stylesheet" href="./files/styles.css?v=1.0">
 <link rel="icon" href="./files/favicon.png" sizes="16x16" type="image/png">
 <!--[if lte IE 7]>
 <link href="//cdn.ubc.ca/clf/7.0.4/css/font-awesome-ie7.css" rel="stylesheet">
@@ -118,10 +118,12 @@ input.addEventListener("keyup", function(event) {
 							<img src="./files/ubc-logo-2018-crest-blue-rgb300.png" alt="" width="100" height="136" />
 						</div>
 						<div class="center l">
-							UBC faculty, staff, and students with a CWL:<a href="/auth/shibboleth/index.php">
+							UBC faculty, staff, and students with a CWL:
 						</div>
 						<div class="center">
-							<img src="./files/cwl_login.png" alt="" width="83" height="35" /></a>
+							<a href="/auth/shibboleth/index.php">
+								<img src="./files/cwl_login.png" alt="" width="83" height="35" />
+							</a>
 						</div>
 					</div>
 					<div class="col">
@@ -133,15 +135,33 @@ input.addEventListener("keyup", function(event) {
 							echo '<div class="error_msg">The Username and/or Password entered is invalid, please try again.</div></br>';}
 						?>
 						<div class="center l" style=" line-height:2.5;">
-								<table border="0">
-									<form name="myForm" action="/login/index.php" method="post">
-									<input type="hidden" name="logintoken" value="<?php echo $login_token;?>" />
-									<tr><td>Username:</td><td><input type="text" name="username"></td></tr>
-									<tr><td>Password:</td><td><input id="passwordBox" type="password" name="password"></td></tr>
-								</table>
-								<div><input id="loginButton" type="button" onclick="login()" value="Login"></div>
-								<div><a href="/login/forgot_password.php">Forgotten your username or password?</a></div>
-								<div style="height:60px; vertical-align:middle;">Cookies must be enabled in your browser</div>
+							<table class="center l" border="0">
+								<form name="myForm" action="/login/index.php" method="post">
+								<input type="hidden" name="logintoken" value="<?php echo $login_token;?>" />
+								<tr><td>Username:</td><td><input type="text" name="username"></td></tr>
+								<tr><td>Password:</td><td><input id="passwordBox" type="password" name="password"></td></tr>
+							</table>
+							<div><input id="loginButton" type="button" onclick="login()" value="Login"></div>
+							<div><a href="/login/forgot_password.php">Forgotten your username or password?</a></div>
+							<div style="height:60px; vertical-align:middle;">
+								Cookies must be enabled in your browser
+							</div>
+							<div class="center l">
+								<h4>Don't have a CWL?</h4>
+							</div>
+							<div class="center howtotext">
+								<p>Hi! For full access to courses you'll need to take a minute to create a new account for yourself on this web site. Here are the steps:</p>
+								<ol>
+									<li>Fill out the New Account form with your details.</li>
+									<li>An email will be immediately sent to your email address.</li>
+									<li>Read your email, and click on the web link it contains.</li>
+									<li>Your account will be confirmed and you will be logged in.</li>
+									<li>Now, select the course you want to participate in.</li>
+								<li>If you are prompted for an "enrolment key" - use the one that your teacher has given you. This will "enrol" you in the course.</li>
+								<li>You can now access the full course. From now on you will only need to enter your personal username and password (in the form on this page) to log in and access any course you have enrolled in.</li>
+								</ol>
+								<input id="crateAccount" type="button" onclick="location.href='/login/signup.php'" value="Create new (non-CWL) account">
+							</div>
 						</div>
 					</div>
 				</div>
